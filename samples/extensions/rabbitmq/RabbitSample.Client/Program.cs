910668c0dafcdf44d9c5213b00f013e48a07ea2c
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-10-08 12:51:20 +0200

    implementation rabbit demo

---
+++ samples/extensions/rabbitmq/RabbitSample.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using CQELight;
+using CQELight.Buses.RabbitMQ.Network;
+using CQELight.Dispatcher;
+using RabbitSample.Common;
+using System;
+
+namespace RabbitSample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trying to connect to RabbitMQ instance @locahost with guest:guest");
+
+            var network = RabbitNetworkInfos.GetConfigurationFor("client", RabbitMQExchangeStrategy.SingleExchange);
+            //This network will produce a client_queue queue, bound to cqelight_global_exchange
+            new Bootstrapper()
+                .UseRabbitMQ(
+                    ConnectionInfosHelper.GetConnectionInfos("client"),
+                    network
+                )
+                .Bootstrapp();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Successfuly connected to RabbitMQ");
+            Console.ResetColor();
+            Console.WriteLine("Enter your message and press enter to send to server, or enter 'quit' to exit process");
+            var data = Console.ReadLine();
+            while (data != "quit")
+            {
+                CoreDispatcher.PublishEventAsync(new NewMessage { Payload = data });
+                data = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ samples/extensions/rabbitmq/RabbitSample.Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using CQELight;
+using CQELight.Buses.RabbitMQ.Network;
+using CQELight.Dispatcher;
+using CQELight.IoC;
+using RabbitSample.Common;
+using System;
+
+namespace RabbitSample.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trying to connect to RabbitMQ instance @locahost with guest:guest");
+
+            var network = RabbitNetworkInfos.GetConfigurationFor("server", RabbitMQExchangeStrategy.SingleExchange);
+            //This network will produce a client_queue queue, bound to cqelight_global_exchange
+            new Bootstrapper()
+                .UseRabbitMQ(
+                    ConnectionInfosHelper.GetConnectionInfos("server"),
+                    network,
+                    cfg => cfg.DispatchInMemory = true
+                )
+                .UseInMemoryEventBus()
+                .UseAutofacAsIoC()
+                .Bootstrapp();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Successfuly connected to RabbitMQ");
+            Console.ResetColor();
+            Console.WriteLine("Listening... Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}

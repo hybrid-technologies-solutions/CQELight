e8348c54b9897de6d69df7f0b19e098a8dff66ac
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2020-06-23 21:33:18 +0200

    create selenium tests

---
+++ tests/CQELight.ASPCore.Tests/CQELightASPNetCore2_1.Tests.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Xunit;
+
+namespace CQELight.ASPCore.Tests
+{
+    public class CQELightASPNetCore2_1Tests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private readonly Process process;
+
+        public CQELightASPNetCore2_1Tests()
+        {
+            _driver = new ChromeDriver(".");
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void ASPNetCore2_1_Should_Run()
+        {
+            _driver.Navigate()
+                   .GoToUrl("http://localhost:9999");
+
+            _driver.Title.Should().StartWith("CQELight demo website");
+            _driver.PageSource.Should().Contain("If you see this, it means that your website is running with CQELight !");
+        }
+    }
+}
